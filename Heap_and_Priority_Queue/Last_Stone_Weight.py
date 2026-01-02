74cfe9420ec43b6f42db466d75a0e97a5c76d1d5
Jaden <jadenmcclary@gmail.com>
2026-01-02 11:25:49 -0600

    More heaps

---
+++ Heap_and_Priority_Queue/Last_Stone_Weight.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        maxHeap = [-x for x in stones]
+        heapq.heapify(maxHeap)
+
+        while len(maxHeap) > 1:
+            first = heapq.heappop(maxHeap)
+            second = heapq.heappop(maxHeap)
+            heapq.heappush(maxHeap, (first - second))
+        return maxHeap[0] * -1
