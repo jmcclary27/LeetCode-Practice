00f7bf71791184081f7923199c90cfbd53b39684
Jaden <jadenmcclary@gmail.com>
2026-06-09 22:25:43 -0500

    Trees

---
+++ Trees/Evaluate_Boolean_Binary_Trees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return
+        
+        if root.left:
+            left = self.evaluateTree(root.left)
+            right = self.evaluateTree(root.right)
+
+            if root.val == 2:
+                return left or right
+            else:
+                return left and right
+        else:
+            if root.val == 0:
+                return False
+            else:
+                return True
