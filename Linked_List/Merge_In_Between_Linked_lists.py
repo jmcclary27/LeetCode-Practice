990d1d6427004b001a52ca494146a9541b156c06
Jaden <jadenmcclary@gmail.com>
2026-06-12 13:31:04 -0500

    Linked Lists

---
+++ Linked_List/Merge_In_Between_Linked_lists.py
@@ -0,0 +1,14 @@
+        ptr = list1
+        for _ in range(a - 1):
+            ptr = ptr.next
+        
+        qtr = ptr.next
+        for _ in range(b - a + 1):
+            qtr = qtr.next
+        
+        ptr.next = list2
+        while list2.next:
+            list2 = list2.next
+        list2.next = qtr
+        
+        return list1
